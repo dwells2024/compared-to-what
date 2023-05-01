c960d8c257e879690b715c29c2424b61c0a38d81
dwells2024 <davidwells2024@u.northwestern.edu>
2023-05-01 15:05:43 -0500

    added react template

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, request, jsonify
-# from flask_cors import CORS
+from flask import Flask, send_from_directory, jsonify, request
+from flask_restful import Api, Resource, reqparse
+#from flask_cors import CORS #comment this on deployment
 import json
-app = Flask(__name__)
+
+
+app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 
 # CORS: allows anyone from anywhere to use your API:
 # cors = CORS(app)
@@ -11,9 +14,9 @@ with open("data/place_indices.json") as file:
 with open("data/100nn_cities.json") as file:
 	knn_cities = json.load(file)
 
-@app.route("/")
+@app.route("/", defaults={'path':''})
 def default():
-	return "Compared to what"
+	return send_from_directory(app.static_folder,'index.html')
 
 @app.route("/all-places")
 def get_places():
