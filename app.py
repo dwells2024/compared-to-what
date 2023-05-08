c79a48c0e8e458c6279ca4a3bd4d9cb749648737
dwells2024 <davidwells2024@u.northwestern.edu>
2023-05-08 00:57:12 -0500

    added filters and latlongs to api

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 # from flask_cors import CORS
 import json
 
@@ -10,21 +11,66 @@ app = Flask(__name__)
 with open("data/place_indices.json") as file:
 	place_indices = json.load(file)
 
-with open("data/100nn_cities.json") as file:
-	knn_cities = json.load(file)
-
 # @app.route("/", defaults={'path':''})
 @app.route("/")
 def default():
 	return "Compared to what"
 
+@app.route("/get-similar")
+def get_similar_cities():
+	place = request.args.get("place")
+	if place == None:
+		return "<p>You must specify a place. Refer to <a href=\"comparedtowhat.azurewebsites.net\">comparedtowhat.azurewebsites.net</a> for examples."
+	if place not in place_indices["places"]:
+		return "Place not found. Make sure you are using the correct census place name and formatting. Refer to <a href=\"comparedtowhat.azurewebsites.net\">comparedtowhat.azurewebsites.net</a> for examples."
+	
+	n = request.args.get("n")
+	if n == None:
+		n = 10
+	elif n.isnumeric():
+		n = int(n)
+		if n > 100:
+			return "<p>n must be an integer (max 100). Refer to <a href=\"comparedtowhat.azurewebsites.net\">comparedtowhat.azurewebsites.net</a> for documentation."
+	else:
+		return "<p>n must be an integer (max 100). Refer to <a href=\"comparedtowhat.azurewebsites.net\">comparedtowhat.azurewebsites.net</a> for documentation."
+	
+	filter = request.args.get("filter")
+	print(filter)
+	if filter == None:
+		filter = "default"
+	if filter + ".json" not in os.listdir("data/knns/"):
+		return "<p>Filter not found. Click <a href=\"comparedtowhat.azurewebsites.net/filters\">here</a> for a list of all filters"
+	path = "data/knns/" + filter + ".json"
+	print(path)
+	
+	with open(path) as file:
+		knn_cities = json.load(file)
+	
+	similar_cities_indices = knn_cities[place_indices["places"][place]][1:n+1]
+	similar_cities_names = [place_indices["indices"][str(index)] for index in similar_cities_indices]
+	
+	return similar_cities_names
+
 @app.route("/all-places")
 def get_places():
 	return jsonify(list(place_indices["places"].keys()))
 
-@app.route("/get-similar")
-def get_similar_cities():
+@app.route("/all-filters")
+def get_filters():
+	return jsonify([path[:-5] for path in os.listdir("data/knns/")])
+
+@app.route("/filters")
+def print_filters():
+	return "".join(["<p>" + path[:-5] + "</p>" for path in os.listdir("data/knns/")])
+
+@app.route("/latlong")
+def get_latlong():
 	place = request.args.get("place")
-	similar_cities_indices = knn_cities[place_indices["places"][place]][1:11]
-	similar_cities_names = [place_indices["indices"][str(index)] for index in similar_cities_indices]
-	return similar_cities_names
+	if place == None:
+		return "<p>You must specify a place. Refer to <a href=\"comparedtowhat.azurewebsites.net\">comparedtowhat.azurewebsites.net</a> for examples."
+	if place not in place_indices["places"]:
+		return "Place not found. Make sure you are using the correct census place name and formatting. Refer to <a href=\"comparedtowhat.azurewebsites.net\">comparedtowhat.azurewebsites.net</a> for examples."
+	
+	with open("data/latlongs.json") as file:
+		data = json.load(file)
+		return data[place]
--- data/misc.py
+++ data/misc.py
@@ -1,22 +1,33 @@
 import csv
 import json
 
-place_list = []
+# place_list = []
 
-with open("us_acs_qf_clean.csv") as csvfile:
-    reader = csv.reader(csvfile)
+# with open("us_acs_qf_clean.csv") as csvfile:
+#     reader = csv.reader(csvfile)
 
-    for row in reader:
-        place_list.append(row)
+#     for row in reader:
+#         place_list.append(row)
 
-place_dict = {
-    "places": {},
-    "indices": {}
-}
+# place_dict = {
+#     "places": {},
+#     "indices": {}
+# }
 
-for i in range(1, len(place_list)):
-    place_dict["places"][place_list[i][0]] = i-1
-    place_dict["indices"][i-1] = place_list[i][0]
+# for i in range(1, len(place_list)):
+#     place_dict["places"][place_list[i][0]] = i-1
+#     place_dict["indices"][i-1] = place_list[i][0]
 
-with open("place_indices.json", 'w') as file:
-    json.dump(place_dict, file)
+# with open("place_indices.json", 'w') as file:
+#     json.dump(place_dict, file)
+
+with open("saved_data.json") as file:
+    data = json.load(file)
+
+latlongs = {}
+
+for place in list(data.values())[1:]:
+    latlongs[place["NAME"]] = [float(place["Latitude"]), float(place["Longitude"])]
+
+with open("latlongs.json", 'w') as file:
+    json.dump(latlongs, file)
