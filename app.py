36631e9223dbf9302889abe0f1abe1003983bb01
dwells2024 <davidwells2024@u.northwestern.edu>
2023-05-22 14:39:29 -0500

    fixed weight link

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ def filter_description():
 	if type(filter_json) != dict:
 		return "Sorry, this filter doesn't have a description."
 	
-	return "<h3>Filter: " + filter + "</h3><p>" + filter_json["description"] + " To see the weights for each metric click <a href=\"/filter-weights?filter=" + filter + "\">here</a>.</p>"
+	return "<h3>Filter: " + filter + "</h3><p>" + filter_json["description"] + " To see the weights for each metric click <a href=\"https://comparedtowhat.azurewebsites.net/filter-weights?filter=" + filter + "\">here</a>.</p>"
 
 @app.route("/latlong")
 def get_latlong():
